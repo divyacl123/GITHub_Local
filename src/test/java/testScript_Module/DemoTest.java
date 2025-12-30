72a36dcc9a69e1dac82e18f8b3ed079354a412dc
Divya C L <Divya C L@DESKTOP-2PHPB8V>
2025-12-30 22:44:23 +0530

    testing for poll scm

--- src/test/java/testScript_Module/DemoTest.java
+++ src/test/java/testScript_Module/DemoTest.java
@@ -18,6 +18,7 @@ public class DemoTest /*extends BaseClass*/ {
 		System.out.println("---demo m2---");
 		System.out.println("Hello hi");
 		System.out.println("I am Divya");
+		System.out.println("Good morning");
 	}
 	
 }
