03e70614136068801c9b5a5ccf1530cc4cd1c46d
Divya C L <Divya C L@DESKTOP-2PHPB8V>
2025-12-31 10:47:30 +0530

    script added for readdata from cmdLine to pass parameter via jenkins

---
+++ src/test/java/testScript_Module/ReadDataFromCmdLine_Test.java
@@ -0,0 +1,53 @@
+package testScript_Module;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class ReadDataFromCmdLine_Test {
+	
+	
+	@Test
+	public void cmdLine() throws InterruptedException {
+		
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		
+		System.out.println(BROWSER);
+		System.out.println(URL);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
+		WebDriver driver=null;
+		if(BROWSER.contentEquals("chrome")) {
+			driver = new ChromeDriver();
+			
+		}
+		else if (BROWSER.equalsIgnoreCase("firefox")) {
+			driver=new FirefoxDriver();
+		}
+		
+		else if (BROWSER.equalsIgnoreCase("edge")) {
+			driver=new EdgeDriver();
+		}
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get(URL);
+		
+		driver.findElement(By.name("user_name")).sendKeys(USERNAME);
+		driver.findElement(By.name("user_password")).sendKeys(PASSWORD);
+		driver.findElement(By.id("submitButton")).click();
+		
+		Thread.sleep(3000);
+		driver.quit();
+		
+	}
+
+}
